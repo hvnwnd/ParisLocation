a9ae78717b5b9512c00b6b35ddb80ee969eab6bf
Bin CHEN <hvnwnd@gmail.com>
2016-02-22 18:27:34 +0100

    Initial Commit

---
+++ LocationParis/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  LocationParis
+//
+//  Created by Bin CHEN on 2/22/16.
+//  Copyright © 2016 Fantestech. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
